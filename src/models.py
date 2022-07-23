3055f0b491cbbd9227d482af63c712d4c6c52c80
NahuelPiecora <nahuelpie@live.com>
2022-07-23 16:49:21 +0000

    final

--- src/models.py
+++ src/models.py
@@ -7,10 +7,8 @@ from sqlalchemy import create_engine
 from eralchemy import render_er
 
 Base = declarative_base()
-
-Base = declarative_base()
 class User(Base):
-    __tablename__ = ‘user’
+    __tablename__ = "user"
     id = Column(Integer, primary_key=True)
     username = Column(String(30), unique=True)
     password = Column(String(16))
@@ -18,25 +16,25 @@ class User(Base):
     firstname = Column(String)
     lastname = Column(String)
 class Character(Base):
-    __tablename__ = ‘character’
+    __tablename__ = "character"
     id = Column(Integer, primary_key=True)
     name = Column(String)
     haircolor = Column(String)
     eyecolor = Column(String)
 class Planet(Base):
-    __tablename__ = ‘planet’
+    __tablename__ = "planet"
     id = Column(Integer, primary_key=True)
     name = Column(String)
     climates = Column(String)
 class Favorite(Base):
-    __tablename__ = ‘favorite’
+    __tablename__ = "favorite"
     id = Column(Integer, primary_key=True)
     character = relationship(Character)
-    character_id = Column(Integer, ForeignKey(‘character.id’), nullable=True)
+    character_id = Column(Integer, ForeignKey('character.id'), nullable=True)
     planet = relationship(Planet)
-    planet_id = Column(Integer, ForeignKey(‘planet.id’), nullable=True)
+    planet_id = Column(Integer, ForeignKey('planet.id'), nullable=True)
     user = relationship(User)
-    user_id = Column(Integer, ForeignKey(‘user.id’))
+    user_id = Column(Integer, ForeignKey('user.id'))
     def validate():
         if not self.planet_id and not self.character_id:
             return False
@@ -47,9 +45,3 @@ class Favorite(Base):
 
 
 
-
-class Character(Base):
-    __tablename__ = 'favorite'
-    id = Column(Integer, primary_key=True)
-## Draw from SQLAlchemy base
-render_er(Base, 'diagram.png')
