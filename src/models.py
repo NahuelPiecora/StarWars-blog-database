e2a71c484550a8e1a70330ba2be6595582cee2ba
NahuelPiecora <nahuelpie@live.com>
2022-07-23 16:56:33 +0000

    final

--- src/models.py
+++ src/models.py
@@ -7,6 +7,7 @@ from sqlalchemy import create_engine
 from eralchemy import render_er
 
 Base = declarative_base()
+
 class User(Base):
     __tablename__ = "user"
     id = Column(Integer, primary_key=True)
